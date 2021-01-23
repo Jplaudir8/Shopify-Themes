d75ddbf4f50812ba0af9827647e31e7253feeb3a
Joan-Perez-Lozano <joanperezl123@gmail.com>
2021-01-23 17:47:07 -0500

    cleaned up JS a bit

--- Project_2/assets/application.js
+++ Project_2/assets/application.js
@@ -1,36 +1,70 @@
 // Put your application javascript here
-$(document).ready(function(){
-    $(document).on('click', '.js-quantity-button', function(event) {
-        // alert('Button Clicked');
-        let
-            $button = $(this),
-            $form = $button.closest('form'),
-            $quantity = $form.find('.js-quantity-field'),
-            quantityValue = parseInt($quantity.val()),
-            max = $quantity.attr('max') ? parseInt($quantity.attr('max')) : null;
+$(document).ready(function() {
 
-            if ($button.hasClass('plus')) {
-                $quantity.val(quantityValue + 1).change();
-            } else if ($button.hasClass('minus')) {
-                $quantity.val(quantityValue - 1).change();
+    let 
+        onQuantityButtonClick = function(event) {
+            // alert('Button Clicked');
+            let
+                $button = $(this),
+                $form = $button.closest('form'),
+                $quantity = $form.find('.js-quantity-field'),
+                quantityValue = parseInt($quantity.val()),
+                max = $quantity.attr('max') ? parseInt($quantity.attr('max')) : null;
+
+                if ($button.hasClass('plus') && (max === null || quantityValue + 1 <= max) ) {
+                    $quantity.val(quantityValue + 1).change();
+                } else if ($button.hasClass('minus')) {
+                    $quantity.val(quantityValue - 1).change();
+                }
+        },
+        onQuantityFieldChange = function(event) {
+            let
+                $field = $(this),
+                $form = $field.closest('form'),
+                $quantityText = $form.find('.js-quantity-text'),
+                shouldDisableMinus = parseInt(this.value) === 1,
+                shouldDisablePlus = parseInt(this.value) === parseInt($field.attr('max')),
+                $minusButton = $form.find('.js-quantity-button.minus'),
+                $plusButton = $form.find('.js-quantity-button.plus');
+    
+            $quantityText.text(this.value);
+                
+            if (shouldDisableMinus) {
+                $minusButton.prop('disabled', true); // .prop() means property
+            } else if ($minusButton.prop('disabled') === true) {
+                $minusButton.prop('disabled', false);
             }
-    });
+    
+            if (shouldDisablePlus) {
+                $plusButton.prop('disabled', true);
+            } else if ($plusButton.prop('disabled') === true) {
+                $plusButton.prop('disabled', false);
+            }
+    
+        },
+        onVariantRadioChange = function(event) {
+            let 
+                $radio = $(this),
+                $form = $radio.closest('form'),
+                max = $radio.attr('data-inventory-quantity'),
+                $quantity = $form.find('.js-quantity-field'),
+                $addToCartButton = $form.find('#add-to-cart-button');
+    
+            if ($addToCartButton.prop('disabled') === true) {
+                $addToCartButton.prop('disabled', false);
+            }
+                
+            $quantity.attr('max', max);
+    
+            if (parseInt($quantity.val()) > max) {
+                $quantity.val(max).change();
+            }
+        };
 
-    $(document).on('change', '.js-quantity-field', function(event) {
-        let
-            $field = $(this),
-            $form = $field.closest('form'),
-            $quantityText = $form.find('.js-quantity-text'),
-            shouldDisabledMinus = parseInt(this.value) === 1,
-            $minusButton = $form.find('.js-quantity-button.minus');
+    $(document).on('click', '.js-quantity-button', onQuantityButtonClick);
 
-        $quantityText.text(this.value);
-            
-        if(shouldDisabledMinus) {
-            $minusButton.prop('disabled', true); // .prop() means property
-        } else if ($minusButton.prop('disabled') === true) {
-            $minusButton.prop('disabled', false);
-        }
-    });
+    $(document).on('change', '.js-quantity-field', onQuantityFieldChange);
+
+    $(document).on('change', '.js-variant-radio', onVariantRadioChange);
 
 });
